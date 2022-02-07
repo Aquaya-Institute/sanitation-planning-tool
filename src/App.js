a5d5bc4f529d4ae6e7445cc8fd3f656508536ef7
Kara Stuart <kara@aquaya.org>
2022-02-07 12:53:58 +0300

    analytics update

--- src/App.js
+++ src/App.js
@@ -10,34 +10,33 @@ import FAQ from "./views/FAQ";
 import Datasets from "./views/Datasets";
 import Privacy from "./views/Privacy";
 import "./App.css";
-import Protect from "react-app-protect";
 import "react-app-protect/dist/index.css";
+import ReactGA from "react-ga4";
+import RouteChangeTracker from "./components/subcomponents/RouteChangeTracker";
+
+ReactGA.initialize("G-4M9J6LZRDQ");
+ReactGA.send("pageview");
 
 function App() {
   return (
-    <Protect
-      sha512="40042129d59da9aeb1f847a98459e489c383ad1a09b868ce6052645fcd654647484fe1af323c74f7cd5d604b3f1b242618d911176a1635eb89aef9fc7e1076be"
-      blur={true}
-      boxTitle="The site will unlock once the webinar is over. Please check back soon!"
-    >
-      <ThemeProvider theme={theme}>
-        <MapContextProvider>
-          <Router>
-            <DefaultLayout>
-              <Switch>
-                <Route path="/maps/:id" component={MapDetail} />
-                <Route path="/maps" component={Home} />
-                <Route path="/about" component={About} />
-                <Route path="/datasets" component={Datasets} />
-                <Route path="/faq" component={FAQ} />
-                <Route exact path="/" component={Home} />
-                <Route exact path="/privacy" component={Privacy} />
-              </Switch>
-            </DefaultLayout>
-          </Router>
-        </MapContextProvider>
-      </ThemeProvider>
-    </Protect>
+    <ThemeProvider theme={theme}>
+      <MapContextProvider>
+        <Router>
+          <RouteChangeTracker />
+          <DefaultLayout>
+            <Switch>
+              <Route path="/maps/:id" component={MapDetail} />
+              <Route path="/maps" component={Home} />
+              <Route path="/about" component={About} />
+              <Route path="/datasets" component={Datasets} />
+              <Route path="/faq" component={FAQ} />
+              <Route exact path="/" component={Home} />
+              <Route exact path="/privacy" component={Privacy} />
+            </Switch>
+          </DefaultLayout>
+        </Router>
+      </MapContextProvider>
+    </ThemeProvider>
   );
 }
 
